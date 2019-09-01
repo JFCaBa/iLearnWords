ad739296329df70f42d7273c2b2588884a8a5b95
José Catalá <jfca68@gmail.com>
2019-09-01 10:02:22 +0100

    Initial Commit

---
+++ iLearnWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iLearnWords
+//
+//  Created by Jose Francisco Catalá Barba on 01/09/2019.
+//  Copyright © 2019 Jose Francisco Catalá Barba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
